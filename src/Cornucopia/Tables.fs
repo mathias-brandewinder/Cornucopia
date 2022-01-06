6e1572a3a4ed950012eb4f87f82582fe81c5ffff
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2022-01-06 11:30:58 -0800

    Update Tables.fs

--- src/Cornucopia/Tables.fs
+++ src/Cornucopia/Tables.fs
@@ -11,7 +11,7 @@ type Entry<'T> =
     | Or of list<Entry<'T>>
     | Table of TableRef
     | Weighted of list<Weight * Entry<'T>>
-    | Repeat of uint * Entry<'T>
+    | Repeat of int * Entry<'T>
     | Merge of (Entry<'T> * Entry<'T>)
 
 type NamedTable<'T> = {
@@ -33,15 +33,28 @@ module Table =
             match entry with
             | Item item -> List.singleton item
             | And entries -> entries |> List.collect (eval ctx)
-            | Or entries -> entries |> Pick.uniform ctx.RNG |> eval ctx
-            | Weighted entries -> entries |> Pick.weighted ctx.RNG |> eval ctx
-            | Table ref -> ctx.Tables.[ref].Entries |> Pick.from ctx.RNG |> eval ctx
-            | Repeat (n, entry) -> List.init (int n) (fun _ -> entry |> eval ctx) |> List.collect id
+            | Or entries ->
+                match entries with
+                | [] -> []
+                | _ ->
+                    entries |> Pick.uniform ctx.RNG |> eval ctx
+            | Weighted entries ->
+                match entries with
+                | [] -> []
+                | _ ->
+                    entries |> Pick.weighted ctx.RNG |> eval ctx
+            | Table ref ->
+                ctx.Tables.[ref].Entries |> Pick.from ctx.RNG |> eval ctx
+            | Repeat (n, entry) ->
+                List.init n (fun _ -> entry |> eval ctx)
+                |> List.collect id
             | Merge (entry1, entry2) ->
-                [
-                    for e1 in eval ctx entry1 do
-                        for e2 in eval ctx entry2 ->
-                            ctx.Merge(e1, e2)
-                ]
+                eval ctx entry1
+                |> List.collect (fun value1 ->
+                    eval ctx entry2
+                    |> List.map (fun value2 ->
+                        ctx.Merge (value1, value2)
+                        )
+                    )
             |> List.distinct
         eval ctx (Table (Ref table))
